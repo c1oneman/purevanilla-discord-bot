a7159886d59ba5b9ffd6829a93a3de369de9187d
BuildTools <clayloneman@gmail.com>
2021-01-23 20:06:52 -0600

    new comp

--- commands/weekly.js
+++ commands/weekly.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 var ServerTap_API = process.env.PUREVANILLA_SERVER_ENDPOINT || 'localhost:25566'
 var key = process.env.API_KEY;
-var Current_Competition = "apple";
+var Current_Competition = "ts_DamageTaken";
 
 module.exports.run = async (bot, message, args) => {
     var unirest = require("unirest");
