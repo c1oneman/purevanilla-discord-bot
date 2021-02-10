293d6b7b3a8d7a4bb05d9745158b4c3932ced3fd
BuildTools <clayloneman@gmail.com>
2021-02-10 12:42:07 -0600

    update status issue

--- commands/status.js
+++ commands/status.js
@@ -7,30 +7,37 @@ var CreativeServerIP = 'creative.purevanilla.net'
 const Discord = require('discord.js')
 
 module.exports.run = async (bot, message, args) => {
+  var final = "";
   mcping(`${ServerIP}`, 25565, function (err, res) {
-    var final = '';
+    
     if (err) {
       console.log(err);
       final = ":red_circle: Pure Vanilla is `offline`";
     } else {
       console.log(res);
       final = ":green_circle: Pure Vanilla is `online` with " + res['num_players'] + " players.\n";
-      mcping(`${CreativeServerIP}`, 25565, function (err, res) {
-        if (err) {
-          console.log(err);
-          final = final + ":red_circle: Pure Vanilla Creative is `offline`";
-        } else {
-          console.log(res);
-          final = final + ":green_circle: Pure Vanilla Creative is `online` with " + res['num_players'] + " players.";
-        }
-        
-      }, 1000);
-
-
     }
-    message.channel.send(final);
+    
   }, 1000);
-  
+  mcping(
+    `${CreativeServerIP}`,
+    25565,
+    function (err, res) {
+      if (err) {
+        console.log(err);
+        final = final + ":red_circle: Pure Vanilla Creative is `offline`";
+      } else {
+        console.log(res);
+        final =
+          final +
+          ":green_circle: Pure Vanilla Creative is `online` with " +
+          res["num_players"] +
+          " players.";
+      }
+    },
+    1000
+  );
+  message.channel.send(final);
 
 }
 
