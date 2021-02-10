0107a88ae62221f05d6220216fc6a4ed10e9175d
BuildTools <clayloneman@gmail.com>
2021-02-10 15:59:33 -0600

    fixed status final

--- commands/status.js
+++ commands/status.js
@@ -7,40 +7,51 @@ var CreativeServerIP = 'creative.purevanilla.net'
 const Discord = require('discord.js')
 
 module.exports.run = async (bot, message, args) => {
-  var final = "";
-  mcping(`${ServerIP}`, 25565, function (err, res) {
-    
-    if (err) {
-      console.log(err);
-      final = ":red_circle: Pure Vanilla is `offline`";
-    } else {
-      console.log(res);
-      final = ":green_circle: Pure Vanilla is `online` with " + res['num_players'] + " players.\n";
-    }
-    
-  }, 1000);
-  mcping(
-    `${CreativeServerIP}`,
-    25565,
-    function (err, res) {
-      if (err) {
-        console.log(err);
-        final = final + ":red_circle: Pure Vanilla Creative is `offline`";
-      } else {
-        console.log(res);
-        final =
-          final +
-          ":green_circle: Pure Vanilla Creative is `online` with " +
-          res["num_players"] +
-          " players.";
-      }
-    },
-    1000
-  );
+  let servers = [
+    { server_ip: "play.purevanilla.net", name: "Pure Vanilla" },
+    { server_ip: "creativee.purevanilla.net", name: "Creative" },
+  ];
+  for(let i = 0; i<servers.length;i++) {
+  servers[i] = {
+    ...servers[i],
+    ...(await getStatus(servers[i].server_ip)),
+  };
+  }
+  
+  let final = ""
+  servers.forEach(server => {
+    final =
+      final +
+      `${server.online ? ":green_circle:" : ":red_circle:"} ${server.name} is ${
+        server.online ? "`online` with " : "`offline`."
+      } ${server.online ? server.players + " players." : ""}\n`;
+  })
   message.channel.send(final);
 
 }
-
+const getStatus = async (server) => {
+  var status = { online: false, players: 0 };
+  await new Promise((resolve, reject) => {
+    mcping(
+    
+    `${server}`,
+    25565,
+    function (err, res) {
+      if (err) {
+        status.online = false
+        status.players = 0
+      } else {
+        status.online = true;
+        status.players = res["num_players"];
+      }
+      resolve (status);
+    },
+    1000
+  );
+  });
+  
+  return status
+};
 module.exports.help = {
   name: "status",
   aliases: ["online"]
