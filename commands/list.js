346c0abbbf424c68b9394843c1041a4cfc9bc3c4
BuildTools <clayloneman@gmail.com>
2021-07-04 20:17:15 -0400

    fix????

--- commands/list.js
+++ commands/list.js
@@ -30,7 +30,7 @@ module.exports.run = (bot, message, args) => {
       );
     } else {
       message.channel.send("Could not reach server.");
-      console.log(res.message);
+      console.log(res);
     }
   });
 };
