9f0d6f5018b15cf2af47fb6b239e2e5661e246e8
BuildTools <clayloneman@gmail.com>
2021-07-23 22:12:01 -0400

    update command!

--- commands/marker.js
+++ commands/marker.js
@@ -10,7 +10,13 @@ module.exports.run = async (interaction, client) => {
 
   const guild = client.guilds.cache.get(interaction.guild_id);
   let embed = new Discord.MessageEmbed();
-  if (isRole(interaction.member, guild, "hasMarked")) {
+  if (
+    isRole(
+      interaction.member,
+      guild,
+      "hasMarked" && !isRole(interaction.member, guild, "Founder")
+    )
+  ) {
     reply(interaction, client, "Sorry this command can only be run once!");
     return;
   }
@@ -24,7 +30,7 @@ module.exports.run = async (interaction, client) => {
   }
 
   const { options } = interaction.data;
-  let roleID = guild.roles.cache.find((role) => role.name === "Member");
+  let roleID = guild.roles.cache.find((role) => role.name === "hasMarked");
   const label = options[0].value;
   const icon = options[1].value;
   const x = options[2].value;
@@ -51,9 +57,11 @@ module.exports.run = async (interaction, client) => {
       `command=dmarker add id:${id} "${label}" world:${world} x:${x} y:0 z:${z} icon:${icon} set:1`
     )
     .send("time=0")
-    .end(function (res) {
+    .end(async function (res) {
       {
-        if (res.status == 200) {
+        if (!res.body.toLowerCase().includes("error")) {
+          var member = await guild.members.fetch(interaction.member.user.id);
+          member.roles.add(roleID.id);
           // Server pinged back
           console.log(res.body);
 
