eeb5d0df08b4123d17e15435ea5fde376330a436
Clayton Loneman <clayloneman@gmail.com>
2021-12-19 11:41:38 -0600

    new week 3

--- music_commands/weekly.js
+++ music_commands/weekly.js
@@ -24,7 +24,7 @@ module.exports = class extends SlashCommand {
 
   async run(ctx) {
     //const { client } = require("..");
-    const week = "2";
+    const week = "3";
     const week_id = ctx.options.week
       ? `week_${ctx.options.week}`
       : `week_${week}`;
